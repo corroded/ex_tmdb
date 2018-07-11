67d388ea8d6c6f6abede2024f80e86878a0699d8
Eumir Gaspar <eumirgaspar@gmail.com>
2018-07-12 04:55:50 +0800

    Initial commit

---
+++ lib/ex_tmdb.ex
@@ -0,0 +1,18 @@
+defmodule ExTmdb do
+  @moduledoc """
+  Documentation for ExTmdb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTmdb.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_tmdb_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTmdbTest do
+  use ExUnit.Case
+  doctest ExTmdb
+
+  test "greets the world" do
+    assert ExTmdb.hello() == :world
+  end
+end
